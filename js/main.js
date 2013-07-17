78db55a88f12384085604eb21af4d4d998c92930
Jessica Kenney <seveneightn9ne@gmail.com>
2013-07-16 20:59:52 -0400

    text and nav structure on home page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var d = new Date();
+var year = d.getFullYear();
+sophomore = new Date("August 31, 2013 00:00:00");
+junior = new Date("May 31, 2014 00:00:00");
+senior = new Date("May 31, 2015 00:00:00");
 
+if (d > sphomore) {
+	grade = "sophomore"
+}
+if (d > junior) {
+	grade = "junior"
+}
+if (d > senior) {
+	grade = "senior"
+}
